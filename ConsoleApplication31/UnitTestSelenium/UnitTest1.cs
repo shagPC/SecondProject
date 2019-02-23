2325bf8c3f1c7880e8ebcc85c81d84c19073df6f
shagPC <47909463+shagPC@users.noreply.github.com>
2019-02-23 11:53:08 +0600

    Add files via upload

---
+++ ConsoleApplication31/UnitTestSelenium/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+namespace UnitTestSelenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private ChromeDriver chrome;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("http://mail.ru");
+            chrome.FindElement(By.Id("q")).SendKeys("Автотесты");
+            chrome.FindElement(By.Id("q")).SendKeys(Keys.Enter);
+        }
+    }
+}
