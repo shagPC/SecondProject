297eb803e6e1b802fb0741aa7d9ef184d0948b21
НупбаевЖ <@410-11>
2019-02-23 12:42:44 +0600

    added numbers compare

--- ConsoleApplication31/ConsoleApplication31/Program.cs
+++ ConsoleApplication31/ConsoleApplication31/Program.cs
@@ -14,6 +14,22 @@ namespace ConsoleApplication31
             Console.WriteLine("second");
             Console.WriteLine("3");
             Console.WriteLine("4");
+
+            int firstNumber;
+            int secondNumber;
+
+            Int32.TryParse(Console.ReadLine(), out firstNumber);
+            Int32.TryParse(Console.ReadLine(), out secondNumber);
+
+            if(firstNumber > secondNumber)
+            {
+                Console.WriteLine("first");
+            }
+            else
+            {
+                Console.WriteLine("second");
+            }
+
         }
     }
 }
