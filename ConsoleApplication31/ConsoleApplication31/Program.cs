e3e20e237fe8fef779b58475522cda50d0c28c33
РахимжановА <@402-1NEW>
2019-03-02 09:09:51 +0600

    new Print

--- ConsoleApplication31/ConsoleApplication31/Program.cs
+++ ConsoleApplication31/ConsoleApplication31/Program.cs
@@ -14,11 +14,22 @@ namespace ConsoleApplication31
             Console.WriteLine("second");
             Console.WriteLine("3");
             Console.WriteLine("4");
+
+            HelloPrint.Print();
         }
 
         public void PrintAskar()
         {
             Console.WriteLine("Hello world!");
+        }
+    }
+    public class HelloPrint
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
         }
     }
 }
