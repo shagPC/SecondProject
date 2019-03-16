32bd96d90fe7c21891fc622ba9e3c7e0127115a8
Аскар <@DESKTOP-Q5Q26T1>
2019-03-16 21:27:25 +0600

    Add and Fix class Employee

---
+++ ConsoleApplication31/ConsoleApplication31/Brigade.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication31
+{
+    public class Brigade
+    {
+        public Employee Brigadier { get; set; }
+        public int NumBrigade { get; private set; }
+        public List<Employee> employees = new List<Employee>
+        {
+            new Employee("Alex", 23, 80000), new Employee("Serg", 34, 120000), new Employee("Mary", 31, 100000)
+        };
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            foreach (var item in employees)
+                yield return item;
+        }
+        public override string ToString()
+        {
+            return $"{NumBrigade} , {Brigadier} {employees.Count}";
+        }
+
+        public Employee this[int index]
+        {
+            get
+            {
+                return employees[index];
+            }
+            set
+            {
+                employees[index] = value;
+            }
+        }
+        public void Add(Employee emp)
+        {
+            employees.Add(emp);
+        }
+        public void AddRange(params Employee [] emps)
+        {
+            //foreach(var item in emps)
+            employees.AddRange(emps);
+        }
+    }
+}
